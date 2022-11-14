9ee5fe0bd6d3771df03e45b1b6bb291234494a17
Fernando Solano <fesolano@outcoding.com>
2022-11-14 12:30:58 -0600

    first commit

---
+++ BouncingBall/Models/Controls/CtrlBouncingBall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BouncingBall.Models.Controls
+{
+    public class CtrlBouncingBall : CtrlBaseModel
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public CtrlBouncingBall()
+        {
+        }
+    }
+}
---
+++ BouncingBall/Models/Helpers/ControlExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BouncingBall.Models.Controls;
+
+namespace BouncingBall.Models.Helpers
+{
+    public static class ControlExtensions
+    {
+        public static HtmlString CtrlBouncingBall(this HtmlHelper html, string id, int height, int width)
+        {
+            var ctrl = new CtrlBouncingBall
+            {
+                Id = id,
+                Width = width,
+                Height = height
+            };
+
+            return new HtmlString(ctrl.GetHtml());
+        }
+    }
+}
